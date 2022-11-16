a3106bbefbc80be2c3355375ee9706ac0d362524
cly11204 <95332853+cly11204@users.noreply.github.com>
2022-11-16 14:27:56 -0500

    discussion 11

--- discussion11.py
+++ discussion11.py
@@ -39,13 +39,17 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
-
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    #creates a new table and add the column headers
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY, name TEXT, species_id NUMBER, age INTEGER, cuteness INTEGER, aggressiveness NUMBER)")
+    #need to commit if you're making changes to the data table 
+    conn.commit()
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
-    
+    # add into our table name
+    cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) values(?,?,?,?,?,?)", (0, "Fluffle", 0, 3, 90, 100))
+    conn.commit()
 
 # TASK 2
 # CODE TO ADD JSON TO THE TABLE
@@ -59,16 +63,30 @@ def add_pets_from_json(filename, cur, conn):
     json_data = json.loads(file_data)
 
     # THE REST IS UP TO YOU
-    pass
-
+    count = 1
+    for data in json_data: 
+        pet_id = count
+        count +=1
+        cur.execute("SELECT id FROM Species WHERE title = ?", (data['species'],))
+        
+        #fetch our results form cur.execute --> return a tuple 
+        species_id = cur.fetchone()[0]
+        name = data['name']
+        age = data['age']
+        cuteness = data['cuteness']
+        aggressiveness = data['aggressiveness']
+        cur.execute("INSERT INTO Patients (pet_id, name, species_id, age, cuteness, aggressiveness) values(?,?,?,?,?,?)", (pet_id, name, species_id, age, cuteness, aggressiveness))
+    conn.commit()
 
 # TASK 3
 # CODE TO OUTPUT NON-AGGRESSIVE PETS
 def non_aggressive_pets(aggressiveness, cur, conn):
+    #cur.execute("SELECT pet_id FROM Patients WHERE aggressiveness < 10", (aggressiveness))
+    #return a list of the tuples 
+    print(cur.fetchall())
+    conn.commit()
+
     pass
-
-
-
 def main():
     # SETUP DATABASE AND TABLE
     cur, conn = setUpDatabase('animal_hospital.db')
